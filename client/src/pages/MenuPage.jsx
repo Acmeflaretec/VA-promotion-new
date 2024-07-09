e6010cf648775fa13cad5cd01893288c0013930d
AnanthuManoj <120579729+AnanthuManoj@users.noreply.github.com>
2024-07-09 20:47:38 +0530

    updated the slider in video and channel promotion page

--- client/src/pages/MenuPage.jsx
+++ client/src/pages/MenuPage.jsx
@@ -1,66 +1,39 @@
 import React from "react";
-import NavBar from "../components/Navbar";
-import { Col, Row } from "react-bootstrap";
-import Personaladd from "../components/Personaladd";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import "./menu.css";
+import NavBar from "../components/Navbar";
+import Personaladd from "../components/Personaladd";
 import Footer from '../components/Footer';
+import "./menu.css";
 
 function MenuPage() {
   return (
-    <div>
+    <div className="d-flex flex-column min-vh-100">
       <NavBar />
-      {/* <div className="banner1">
-      </div> */}
-      <div className="container mt-5 ">
-        <Row>
-          {/* <Col md={12}>
-            <div className="float-end">
-              <div className="btn btn-danger rounded-circle p-3 mb-5 ">
-                channel <br /> profile
-              </div>
-            </div>
-          </Col> */}
-          <Col>
-            <div className="d-flex flex-column flex-md-row justify-content-around">
-            <Link to={'/promotion'}>
-                  <button className="btn btn-secondary rounded-pill p-3 px-5 mb-3 col-12 col-md-auto">
-                    Get Channel Promotion
-                  </button>
-            </Link>
-            <Link to={'/vidiopromotion'}>
-              <button className="btn btn-secondary rounded-pill p-3 mb-3 px-5 col-12 col-md-auto">
-                Get Video Promotion
-              </button>
+      <Container className="flex-grow-1 my-5">
+        <Row className="justify-content-center">
+          <Col md={8} lg={6}>
+            <h1 className="text-center mb-5">Promotion Services</h1>
+            <div className="d-grid gap-4">
+              <Link to="/promotion" className="text-decoration-none">
+                <Button variant="primary" size="lg" className="w-100 py-3">
+                  Channel Promotion
+                </Button>
+              </Link>
+              <Link to="/vidiopromotion" className="text-decoration-none">
+                <Button variant="secondary" size="lg" className="w-100 py-3">
+                  Video Promotion
+                </Button>
               </Link>
             </div>
-            {/* <div className="mt-5">
-              <div className="text-center">
-                <h2> Promotional Policy</h2>
-              </div>
-              <div className="d-flex justify-content-center">
-                <div class="form-check">
-                  <input
-                    class="form-check-input"
-                    type="checkbox"
-                    value=""
-                    id=""
-                    disabled
-                  />
-                  <label class="form-check-label" for="">
-                    {" "}
-                    Agreed with policy{" "}
-                  </label>
-                  
-                </div>
-              </div>
-            </div> */}
           </Col>
         </Row>
-        <div>
-          <Personaladd />
-        </div>
-      </div>
+        <Row className="mt-5">
+          <Col>
+            <Personaladd />
+          </Col>
+        </Row>
+      </Container>
       <Footer />
     </div>
   );
