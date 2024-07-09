49490b636f673436c60e8779d1908e89f8d1a4fb
abdusamad8575 <samadpts786313@gmail.com>
2024-07-09 23:16:51 +0530

    add new selector

--- admin/src/pages/Blogs/tableData.js
+++ admin/src/pages/Blogs/tableData.js
@@ -1,83 +1,3 @@
-// /* eslint-disable react/prop-types */
-// import Box from "components/Box";
-// import Typography from "components/Typography";
-// import Table from "examples/Tables/Table";
-// import { Avatar, Icon } from "@mui/material";
-// import Badge from "components/Badge";
-// import { Link } from "react-router-dom";
-// import { useGetBlogs } from "queries/StoreQuery";
-
-// function Blogs({ image, name }) {
-//   return (
-//     <Box display="flex" alignItems="center" px={1} py={0.5}>
-//       <Box>
-//         <Avatar src={image} alt={name} size="sm" variant="rounded" />
-//       </Box>
-//       {/* <Box display="flex" flexDirection="column">
-//         <Typography variant="button" fontWeight="medium">
-//           {name}
-//         </Typography>
-//         <Typography variant="caption" color="secondary">
-//           {desc}
-//         </Typography>
-//       </Box> */}
-//     </Box>
-//   );
-// }
-
-// const TableData = () => {
-//   const { data, isLoading } = useGetBlogs({ pageNo: 1, pageCount: 100 });
-//   const columns = [
-//     { name: "title", align: "center" },
-//     { name: "subtitle", align: "center" },
-//     { name: "url", align: "center" },
-//     { name: "description", align: "center" },
-//     { name: "image", align: "center" },
-//     { name: "status", align: "center" },
-//     { name: "action", align: "center" },
-//   ]
-
-//   const rows = data?.data?.map(item => ({
-//     title: (
-//       <Typography variant="caption" color="secondary" fontWeight="medium">
-//        {item?.title}
-//       </Typography>
-//     ),
-//     subtitle: (
-//       <Typography variant="caption" color="secondary" fontWeight="medium">
-//         {item?.subtitle}
-//       </Typography>
-//     ),
-//     url: (
-//       <Typography variant="caption" color="secondary" fontWeight="medium">
-//         <a href={item?.url}>{item?.url}</a>
-//       </Typography>
-//     ),
-//     description: (
-//       <Typography variant="caption" color="secondary" fontWeight="medium" >
-//         {item?.description}
-//       </Typography>
-//     ),
-//     image: <Blogs image={`${process.env.REACT_APP_API_URL}/uploads/${item?.image}`} />,
-    
-//     status: (
-//       <Badge variant="gradient" badgeContent={item?.status ? 'Active' : 'Blocked'} color={item?.status ? "success" : 'secondary'} size="xs" container />
-//     ),
-    
-//     action: (
-//       <Link to={`/ads/editAds/${item?._id}`}>
-//         <Icon sx={{ cursor: "pointer", fontWeight: "bold" }} fontSize="small">
-//           more_vert
-//         </Icon>
-//       </Link>
-//     ),
-//   }))
-//   return isLoading ? <Typography fontSize={14} sx={{ paddingX: 5 }}>loading...</Typography> : <Table columns={columns} rows={rows} />
-// };
-
-// export default TableData;
-
-
 
 import React from "react";
 import Box from "components/Box";
@@ -89,11 +9,11 @@ import { Link } from "react-router-dom";
 import { useGetBlogs } from "queries/StoreQuery";
 import "./TableData.css"; 
 
-function Blogs({ image, name }) {
+function Blogs({ image }) {
   return (
     <Box display="flex" alignItems="center" px={1} py={0.5}>
       <Box>
-        <Avatar src={image} alt={name} size="sm" variant="rounded" />
+        <Avatar src={image} alt={'image'} size="sm" variant="rounded" />
       </Box>
     </Box>
   );
@@ -133,7 +53,7 @@ const TableData = () => {
         {item?.description}
       </Typography>
     ),
-    image: <Blogs image={`${process.env.REACT_APP_API_URL}/uploads/${item?.image}`} name={item?.title} />,
+    image: <Blogs image={`${process.env.REACT_APP_API_URL}/uploads/${item?.image}`} />,
     status: (
       <Badge variant="gradient" badgeContent={item?.status ? 'Active' : 'Blocked'} color={item?.status ? "success" : 'secondary'} size="xs" container />
     ),
